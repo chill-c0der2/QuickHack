6693d8f67a61a03e8d73ee42b58f88a84d118537
chill-c0der <99861040+chill-c0der2@users.noreply.github.com>
2022-12-05 14:57:05 -0800

    Update QuickHack.sh

--- QuickHack.sh
+++ QuickHack.sh
@@ -1,6 +1,6 @@
 base64 -d <<<"ICAoKCAgICAoICAgICAoICAoICAgKCAoICAgKCAgICggICggICAKICApKVwgICApXCAgKCAoXCApXCAgKVwpXDogKVwgIChcIClcICAKICgoXykpICgoXykgKVwoXCgoXykoXykoXykoXygpIChcKChfKSAKKC8gXyBcKChfKSkoXykoICkgfF9fIHx8IHwoXykoKSggKSB8X18KfCAoXykgfCB8fCB8IHwgX3wgLyAvIF9fIHwgX2AgfCBffCAvIC8KIFxfX1xfXFxfLl98X3xfX3xfXF9cX3x8X3xfXy9ffF9ffF9cX1wK"
 PS3='linux@QuickHack '
-options=("Setup Listner" "Reverse Shell Generator" "Option 3" "Quit")
+options=("Setup Listner" "Reverse Shell Generator" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -8,11 +8,7 @@ do
             nc –nlvp 8080
             ;;
         "Reverse Shell Generator")
-            cd Shelly
             bash shelly.sh
-            ;;
-        "Option 3")
-            echo "you chose choice $REPLY which is $opt"
             ;;
         "Quit")
             break
